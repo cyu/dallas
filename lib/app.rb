c677d4f1e3bf1bfff4950fa096bb31c6a10c7351
Calvin Yu <me@sourcebender.com>
2017-03-12 01:00:10 -0500

    Fix require for dropbox2 auth

--- lib/app.rb
+++ lib/app.rb
@@ -4,7 +4,7 @@ require 'dotenv/load'
 require 'incoming/dallas/models'
 
 require 'omniauth/strategies/dallas'
-require 'omniauth-dropbox'
+require 'omniauth-dropbox2'
 
 DataMapper::Logger.new($stdout, (ENV['DATAMAPPER_LOG_LEVEL'] || :info).to_sym)
 DataMapper.setup(:default, ENV['POSTGRES_URL'])
